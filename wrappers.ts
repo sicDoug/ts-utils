12c8cf8f068c3635d1c6b1fe61fceb1ee0644585
sicDoug <nouveauso@protonmail.com>
2023-11-13 15:16:30 +0000

    sync

--- wrappers.ts
+++ wrappers.ts
@@ -1,35 +1,3 @@
-// wrappers.ts
-//
-// TypeScript implementation of wrappers inspired by (but not identical to)
-// some Enums found in functional programming languages.
-//
-// The goal of this little library is to provide a protective layer to
-// ensure that one is never unexplicitly working with null, unidentified
-// or errors and always have a safe and idiomatic way to return results.
-//
-// The classes and method have different names than they do in Rust, for
-// example. This is partly to avoid shadowing built-in classes and methods
-// in JS, but mainly to work as a reminder that they do not necessarily
-// behave exactly like their functional language counterparts.
-//
-// The `Outcome` wrapper consists of both a `Success` and `Failure` class,
-// each of which can hold a value. The `Envelope` wrapper consists of
-// `Something` and `Nothing`, of which only `Something` can hold a value.
-//
-// The `unpack()` method retrives the value end ensures that it belongs to a
-// "positive" result, and will throw an exception before ever returning an
-// unexpected result. This is only ever meant to be used after it has been
-// made explicitly clear that the instance is a "positive" one.
-//
-// The `unmask()` method works the same way for retriving the error values.
-// It too will throw an execption if used by mistake on an unintended instance.
-//
-// Both types have mirrored checkers in each class, for example the `success()`
-// method in both instances of `Outcome`. They also have their respective
-// inversions, for example the `Success` instance have both a `success()` method
-// and a `failure()` method. This is to allow for the clearest semantic flow
-// when applied in real life code.
-
 const NO_VALUE = Symbol('No Value');
 
 export type Outcome<T, E> = Success<T> | Failure<E>;
@@ -40,7 +8,6 @@ export class Success<T = typeof NO_VALUE> {
     constructor(value: T | typeof NO_VALUE = NO_VALUE) {
         this._value = value;
     }
-
     unpack(): T {
         if (this._value !== NO_VALUE) {
             return this._value;
@@ -48,16 +15,20 @@ export class Success<T = typeof NO_VALUE> {
             throw new Error("Called `unpack()` on an intance of `Success` which has no value");
         }
     }
-
+    unpackOr(defaultValue: T): T {
+        if (this._value !== NO_VALUE) {
+            return this._value;
+        } else {
+            return defaultValue;
+        }
+    }
     unmask(): never {
         throw new Error("Called `unmask()` on an instance of `Success`");
     }
-
-    success(): boolean {
+    isSuccess(): boolean {
         return true;
     }
-
-    failure(): boolean {
+    isFailure(): boolean {
         return false;
     }
 }
@@ -68,11 +39,12 @@ export class Failure<E = typeof NO_VALUE> {
     constructor(value: E | typeof NO_VALUE = NO_VALUE) {
         this._value = value;
     }
-
     unpack(): never {
         throw new Error("Called `unpack()` on an instance of `Failure`");
     }
-
+    unpackOr<T>(defaultValue: T): T {
+        return defaultValue;
+    }
     unmask(): E {
         if (this._value !== NO_VALUE) {
             return this._value;
@@ -80,12 +52,10 @@ export class Failure<E = typeof NO_VALUE> {
             throw new Error("Called `unmask()` on an instace of `Failure` which has no value");
         }
     }
-
-    success(): boolean {
+    isSuccess(): boolean {
         return false;
     }
-
-    failure(): boolean {
+    isFailure(): boolean {
         return true;
     }
 }
@@ -98,16 +68,16 @@ export class Something<T> {
     constructor(value: T) {
         this._value = value;
     }
-
     unpack(): T {
         return this._value;
     }
-
-    something(): boolean {
+    unpackOr(_value: T): T {
+        return this._value;
+    }
+    isSomething(): boolean {
         return true;
     }
-
-    nothing(): boolean {
+    isNothing(): boolean {
         return false;
     }
 }
@@ -116,12 +86,13 @@ export class Nothing {
     unpack(): never {
         throw new Error("Called `unpack()` on an instance of `Nothing`");
     }
-
-    something(): boolean {
+    unpackOr<T>(defaultValue: T): T {
+        return defaultValue;
+    }
+    isSomething(): boolean {
         return false;
     }
-
-    nothing(): boolean {
+    isNothing(): boolean {
         return true;
     }
 }
